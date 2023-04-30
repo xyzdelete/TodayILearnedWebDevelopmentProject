daa88c354975538ba2447100c32e0c8ff2879cd3
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-30 16:51:18 +0300

    Working With Forms the React Way

--- src/App.js
+++ src/App.js
@@ -95,10 +95,6 @@ function Header({ showForm, setShowForm }) {
     );
 }
 
-function NewFactForm() {
-    return <form className="fact-form">Fact form</form>;
-}
-
 const CATEGORIES = [
     { name: "technology", color: "#3b82f6" },
     { name: "science", color: "#16a34a" },
@@ -109,6 +105,53 @@ const CATEGORIES = [
     { name: "history", color: "#f97316" },
     { name: "news", color: "#8b5cf6" }
 ];
+
+function NewFactForm() {
+    const [text, setText] = useState("");
+    const [source, setSource] = useState("");
+    const [category, setCategory] = useState("");
+    const textLength = text.length;
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(text, source, category);
+    }
+
+    return (
+        <form
+            className="fact-form"
+            onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Share a fact with the world..."
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+
+            <span>{200 - text.length}</span>
+
+            <input
+                value={source}
+                type="text"
+                placeholder="Trustworthy source..."
+                onChange={(e) => setSource(e.target.value)}
+            />
+            <select
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}>
+                <option value="">Choose category:</option>
+                {CATEGORIES.map((categories) => (
+                    <option
+                        key={categories.name}
+                        value={categories.name}>
+                        {categories.name.toUpperCase()}
+                    </option>
+                ))}
+            </select>
+            <button className="btn btn-large">Post</button>
+        </form>
+    );
+}
 
 function CategoryFilter() {
     return (
