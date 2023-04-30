f1eb01471f32ecd429e26a45c397617e95058a24
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-30 15:32:21 +0300

    Rendering the List of Categories

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import "./style.css";
 
-const CATEGORIES = [
-    { name: "technology", color: "#3b82f6" },
-    { name: "science", color: "#16a34a" },
-    { name: "finance", color: "#ef4444" },
-    { name: "society", color: "#eab308" },
-    { name: "entertainment", color: "#db2777" },
-    { name: "health", color: "#14b8a6" },
-    { name: "history", color: "#f97316" },
-    { name: "news", color: "#8b5cf6" }
-];
-
 const initialFacts = [
     {
         id: 1,
@@ -76,8 +65,39 @@ function NewFactForm() {
     return <form className="fact-form">Fact form</form>;
 }
 
+const CATEGORIES = [
+    { name: "technology", color: "#3b82f6" },
+    { name: "science", color: "#16a34a" },
+    { name: "finance", color: "#ef4444" },
+    { name: "society", color: "#eab308" },
+    { name: "entertainment", color: "#db2777" },
+    { name: "health", color: "#14b8a6" },
+    { name: "history", color: "#f97316" },
+    { name: "news", color: "#8b5cf6" }
+];
+
 function CategoryFilter() {
-    return <aside>CategoryFilter</aside>;
+    return (
+        <aside>
+            <ul>
+                <li className="category">
+                    <button className="btn btn-all-categories">All</button>
+                </li>
+
+                {CATEGORIES.map((category) => (
+                    <li
+                        key={category.name}
+                        className="category">
+                        <button
+                            className="btn btn-category"
+                            style={{ backgroundColor: category.color, color: "#eef2ff" }}>
+                            {category.name}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </aside>
+    );
 }
 
 function FactList() {
