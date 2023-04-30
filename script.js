d508bfd6ff3415639545c6a573d997bf00f1223a
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-30 10:13:53 +0300

    Displaying and Hiding the Form

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello World!");
+
+const btn = document.querySelector(".btn-open");
+
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+    if (form.classList.contains("hidden")) {
+        form.classList.remove("hidden");
+        btn.textContent = "close";
+    } else {
+        form.classList.add("hidden");
+        btn.textContent = "share a fact";
+    }
+});
