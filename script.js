6634bf80b515d4231ac5bd7dc5b576e5ff6c1b78
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-30 11:20:05 +0300

    Arrow Functions

--- script.js
+++ script.js
@@ -13,61 +13,13 @@ function calcFactAge(year) {
 
 const age1 = calcFactAge(2015);
 console.log(age1);
-
 console.log(calcFactAge(2020));
 console.log(calcFactAge(1990));
 console.log(calcFactAge(2037));
 
-const btn = document.querySelector(".btn-open");
-const form = document.querySelector(".fact-form");
-
-btn.addEventListener("click", function () {
-    if (form.classList.contains("hidden")) {
-        form.classList.remove("hidden");
-        btn.textContent = "close";
-    } else {
-        form.classList.add("hidden");
-        btn.textContent = "share a fact";
-    }
-});
-
-let votesInteresting = 20;
-let votesMindblowing = 0;
-
-console.log(votesInteresting === votesMindblowing);
-if (votesInteresting === votesMindblowing) {
-    alert("This fact is equally interesting and mindblowing");
-} else if (votesInteresting > votesMindblowing) {
-    console.log("Interesting fact!");
-} else if (votesInteresting < votesMindblowing) {
-    console.log("Mindblowing fact!!");
-}
-
-// falsy values: 0, "", null, undefined
-// truthy value: everything else
-if (votesMindblowing) {
-    console.log("Mindblowing fact!!");
-} else {
-    console.log("Not so special...");
-}
-
-let votesFalse = 7;
-const totalUpvotes = votesInteresting + votesMindblowing;
-
-const message =
-    totalUpvotes > votesFalse
-        ? "The fact is true"
-        : "Might be false, check more sources...";
-
-// alert(message);
-
-const text = "Lisbon is the capital of Portugal";
-const upperText = text.toUpperCase();
-console.log(upperText);
-
-const str = `The current fact "${text}". It is ${calcFactAge(
-    2015
-)} years old. It is probably ${
-    totalUpvotes > votesFalse ? "correct" : "not true"
-}`;
-console.log(str);
+const calcFactAge2 = (year) =>
+    year <= new Date().getFullYear()
+        ? new Date().getFullYear() - year
+        : `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}`;
+console.log(calcFactAge2(2015));
+console.log(calcFactAge2(2037));
