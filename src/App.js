f1f8d44b87349f624834ec807715943726eaf1d1
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-30 20:33:38 +0300

    Finishing Touches and Creating a Production Build

--- src/App.js
+++ src/App.js
@@ -3,54 +3,6 @@ import supabase from "./supabase";
 
 import "./style.css";
 
-const initialFacts = [
-    {
-        id: 1,
-        text: "React is being developed by Meta (formerly facebook)",
-        source: "https://opensource.fb.com/",
-        category: "technology",
-        votesInteresting: 24,
-        votesMindblowing: 9,
-        votesFalse: 4,
-        createdIn: 2021
-    },
-    {
-        id: 2,
-        text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-        source: "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-        category: "society",
-        votesInteresting: 11,
-        votesMindblowing: 2,
-        votesFalse: 0,
-        createdIn: 2019
-    },
-    {
-        id: 3,
-        text: "Lisbon is the capital of Portugal",
-        source: "https://en.wikipedia.org/wiki/Lisbon",
-        category: "society",
-        votesInteresting: 8,
-        votesMindblowing: 3,
-        votesFalse: 1,
-        createdIn: 2015
-    }
-];
-
-// function Counter() {
-//     const [count, setCount] = useState(8);
-
-//     return (
-//         <div>
-//             <span style={{ fontSize: "40px" }}>{count}</span>
-//             <button
-//                 className="btn btn-large"
-//                 onClick={() => setCount((c) => c + 1)}>
-//                 +1
-//             </button>
-//         </div>
-//     );
-// }
-
 function App() {
     // 1. Define state variable
     const [showForm, setShowForm] = useState(false);
@@ -166,7 +118,7 @@ function isValidHttpUrl(string) {
 
 function NewFactForm({ setFacts, setShowForm }) {
     const [text, setText] = useState("");
-    const [source, setSource] = useState("http://example.com");
+    const [source, setSource] = useState("");
     const [category, setCategory] = useState("");
     const [isUploading, setIsUploading] = useState(false);
     const textLength = text.length;
@@ -310,6 +262,7 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
     const [isUpdating, setIsUpdating] = useState(false);
+    const isDisputed = fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
     async function handleVote(columnName) {
         setIsUpdating(true);
@@ -331,6 +284,7 @@ function Fact({ fact, setFacts }) {
     return (
         <li className="fact">
             <p>
+                {isDisputed ? <span className="disputed">[⛔ DISPUTED]</span> : undefined}
                 {fact.text}
                 <a
                     className="source"
